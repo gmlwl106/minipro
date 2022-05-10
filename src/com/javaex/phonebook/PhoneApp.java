716a2795e678deb9bf51e62ba1cacd07f98d9274
gmlwl106 <66778894@naver.com>
2022-05-10 11:18:56 +0900

    2022.05.10. 미니프로젝트 -리스트추가

--- src/com/javaex/phonebook/PhoneApp.java
+++ src/com/javaex/phonebook/PhoneApp.java
@@ -1,9 +1,12 @@
 package com.javaex.phonebook;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -14,70 +17,100 @@ public class PhoneApp {
 		
 		Reader fr = new FileReader("./PhoneDB.txt");
 		BufferedReader br = new BufferedReader(fr);
+		Writer fw = new FileWriter("./PhoneDB-copy.txt");
+		BufferedWriter bw = new BufferedWriter(fw);
+		
 		
 		Scanner sc = new Scanner(System.in);
-		
+		boolean flag = true;
 		List<Phone> pList = new ArrayList<Phone>();
-		while(true) {
-			String readDb = br.readLine();
-			if(readDb == null) {
-				break;
-			}
-			String[] sp = readDb.split(",");
-			pList.add(new Phone(sp[0],sp[1],sp[2]));
-		}
+		
 		
 		System.out.println("**********************************************");
 		System.out.println("*           전화번호 관리 프로그램           *");
 		System.out.println("**********************************************");
-		System.out.println();
 		
-		
-		System.out.println("1.리스트   2.등록   3.삭제   4.검색   5.종료");
-		System.out.println("----------------------------------------------");
-		System.out.print(">메뉴번호: ");
-		int num = sc.nextInt();
-		
-		switch(num) {
-			case 1: //전체 리스트 출력
-				int i = 1;
-				for(Phone p : pList) {
-					System.out.println(i+".   "+p.showPhone());
-					i++;
+		while(flag) {
+			
+			//PhoneDB를 읽어와서 List에 입력
+			
+			while(true) {
+				String readDb = br.readLine();
+				if(readDb == null) {
+					break;
 				}
-				
-				
-				
-				
-				break;
-				
-				
-			case 2: //리스트에 등록
-				break;
-				
-				
-			case 3: //리스트에서 삭제
-				break;
-				
-				
-			case 4: //리스트에서 검색
-				break;
-				
-				
-			case 5: //종료
-				System.out.println("**********************************************");
-				System.out.println("*                 감사합니다                 *");
-				System.out.println("**********************************************");
-				
-				break;
-				
-				
-			default: //없는 메뉴 입력했을때
-				System.out.println("[다시 입력해 주세요.]");
-				break;
+				String[] sp = readDb.split(",");
+				pList.add(new Phone(sp[0],sp[1],sp[2]));
+			}
+			
+			System.out.println();
+			System.out.println("1.리스트   2.등록   3.삭제   4.검색   5.종료");
+			System.out.println("----------------------------------------------");
+			System.out.print(">메뉴번호: ");
+			int num = sc.nextInt();
+			
+			switch(num) {
+				case 1: //전체 리스트 출력
+					
+					int i = 1;
+					for(Phone p : pList) {
+						System.out.println(i+".   "+p.showPhone());
+						i++;
+					}
+					
+					break;
+					
+					
+				case 2: //리스트에 등록
+					sc.nextLine();
+					System.out.print(">이름: ");
+					String name = sc.nextLine();
+					System.out.print(">휴대전화: ");
+					String hp = sc.nextLine();
+					System.out.print(">회사전화: ");
+					String company = sc.nextLine();
+					
+					pList.add(new Phone(name,hp,company)); //pList에 추가
+					
+					//PhoneDB에 추가
+					for(Phone p : pList) {
+						
+						String str = p.getName()+","+p.getHp()+","+p.getCompany();
+						System.out.println("이름: " + p.getName());
+						System.out.println("핸드폰: " + p.getHp());
+						System.out.println("회사: " + p.getCompany());
+						bw.write(str);
+						bw.newLine();//줄바꿈
+						
+					}
+					bw.close();
+					System.out.println("[등록되었습니다.]");
+					break;
+					
+					
+				case 3: //리스트에서 삭제
+					break;
+					
+					
+				case 4: //리스트에서 검색
+					break;
+					
+					
+				case 5: //종료
+					System.out.println("**********************************************");
+					System.out.println("*                 감사합니다                 *");
+					System.out.println("**********************************************");
+					flag = false;
+					break;
+					
+					
+				default: //없는 메뉴 입력했을때
+					System.out.println("[다시 입력해 주세요.]");
+					break;
+			}
 		}
-		
-		
+		sc.close();
+		br.close();
 		
 	}
 
